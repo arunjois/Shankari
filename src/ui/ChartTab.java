49e1a7b244456697717b3c33da5298b9ad6e344b
Jois Jyotisha <joisjyotisha@gmail.com>
2018-12-10 11:47:06 +0530

    [PATCH] Canvas Chart Intergrated

--- src/ui/ChartTab.java
+++ src/ui/ChartTab.java
@@ -1,17 +1,27 @@
 package ui;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.GridPane;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.geometry.*;
+import javafx.scene.control.Label;
 import javafx.scene.Node;
 public class ChartTab {
+    /*
+    * Maybe this method needs a rewrite
+     */
     public TabPane getTab () {
-        TabPane Horo = new TabPane();
-        TabPane Details = new TabPane();
-        Tab Charts =  new Tab("Charts");
-        Tab Info = new Tab("Details");
+        TabPane Horo = new TabPane();               //Parent TabPane
+        TabPane Details = new TabPane();            //Child TabPane
+        Tab Charts =  new Tab("Charts");        //Parent Tab
+        Tab Info = new Tab("Details");          //Child Tab
         StackPane myPane = new StackPane();
         myPane.getChildren().add(getnode());
-        Charts.setContent(myPane);
+        //Charts.setContent(myPane);
+        Info.setContent(myPane);
         Details.getTabs().addAll(Info);
         Charts.setContent(Details);
 
@@ -23,8 +33,48 @@ public class ChartTab {
         return Horo;
     }
     public Node getnode() {
-        StackPane node = new StackPane();
+        Canvas canvas = new Canvas();
+        canvas.setWidth(800);
+        canvas.setHeight(500);
 
-        return node;
+        // Get the graphics context of the canvas
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        // Draw a Text
+        //gc.strokeText("Hello Canvas", 150, 100);
+
+        // Create the Pane
+        Pane root = new Pane();
+        // Set the Style-properties of the Pane
+		/*root.setStyle("-fx-padding: 10;" +
+		  "-fx-border-style: solid inside;" +
+		  "-fx-border-width: 2;" +
+		  "-fx-border-insets: 5;" +
+		  "-fx-border-radius: 5;" +
+		  "-fx-border-color: blue;");
+		  */
+        // Add the Canvas to the Pane
+
+        gc.strokeRect(10,10,780,470);     //Rectangle Box
+        gc.strokeLine(10,115,790,115);    //Line Stroke
+        gc.strokeLine(10,370,790,370);
+        gc.strokeLine(10,235,150,235);    //Horizontal Left lines
+        gc.strokeLine(650,235,790,235);   //Horizontal Right lines
+        gc.strokeLine(385,10,385,115);    //Vertical Top Line
+        gc.strokeLine(385,370,385,480);   //Vertical Bottom Line
+        gc.strokeLine(150,10,150,480);
+        gc.strokeLine(650,10,650,480);     //Vertical Line Long
+        final Label title = new Label("Some text");
+        //title.setMaxWidth(Double.MAX_VALUE);
+        title.setAlignment(Pos.CENTER);
+
+
+
+
+
+        root.getChildren().add(canvas);
+
+
+        return (Node)root;
     }
 }
